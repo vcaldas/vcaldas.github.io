90c39f647628342b8c713d2b3442e0d7c6b93787
Victor Caldas <caldas.victor@gmail.com>
2017-04-03 17:45:55 +0200

    Site updated at 2017-04-03 15:45:55 UTC

--- _scripts/tests.sh
+++ _scripts/tests.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
-set -e
-gulp build:test
-#bundle exec scss-lint _assets/styles/scss/*/*.scss
-#bundle exec mdl . -c .mdlrc --git-recurse
-#bundle exec rake test -f Rakefile
+Script started on Mon Apr  3 17:44:39 2017
+[?1034hbash-3.2$ ./[K[Kbst[K[Kash tests.sh 
+Script started, output file is started
+Script: on: No such file or directory
+
+Script done, output file is started
+bash-3.2$ ls
+build.back.sh	build.sh	started		tests.sh
+bash-3.2$ 
