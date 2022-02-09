ab5f0c35aa72a69888566f623772e5cd31a7acda
VictorCaldas <victor_caldas@cargill.com>
2022-02-09 10:51:42 +0100

    Complete rebuild

--- website/blog/posts/2022/02/automation.md
+++
@@ -1,7 +0,0 @@
-title: Hello World!
-date: 2022-02-08
-category: posts
-published: true
-summary: This is a test post
-
-Finally managing a very simple post.
--- website/main/routes.py
+++ website/main/routes.py
@@ -5,9 +5,4 @@ from website.main import bp
 
 @bp.route("/")
 def main():
-    return render_template("landing.html")
-
-
-@bp.route("/publications")
-def publications():
-    return render_template("main/publications.html")
+    return render_template("about.html")
